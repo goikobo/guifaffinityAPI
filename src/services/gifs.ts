784ab6e1d0cf48d39c44a02f91536cb1e7d019ae
bortxit <bortxit@gmail.com>
2023-06-20 18:04:22 +0200

    fix: limitar resultados de search a 50

--- src/services/gifs.ts
+++ src/services/gifs.ts
@@ -1,19 +1,21 @@
-import Lowdb, { LowdbSync } from "lowdb"
-import { DatabaseSchema } from "../interfaces/DatabaseSchema"
-import { Gif } from "../interfaces/Gif"
+import Lowdb, { LowdbSync } from "lowdb";
+import { DatabaseSchema } from "../interfaces/DatabaseSchema";
+import { Gif } from "../interfaces/Gif";
 
 class GifsService {
   constructor(private db: LowdbSync<DatabaseSchema>) {}
 
   public get() {
-    return this.db.get("gifs").take(50).value()
+    return this.db.get("gifs").take(50).value();
   }
 
   public search(text: string) {
-    const gifs: Gif[] = this.db.get("gifs").value()
-    console.log(gifs.length)
-    return gifs.filter((gif) => gif.title.toLowerCase().includes(text))
+    const gifs: Gif[] = this.db.get("gifs").value();
+
+    return gifs
+      .filter((gif) => gif.title.toLowerCase().includes(text))
+      .slice(0, 50);
   }
 }
 
-export default GifsService
+export default GifsService;
