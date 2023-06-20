dd4871a23cd280e821a6b5fe2b6c3fa985ba412d
bortxit <bortxit@gmail.com>
2023-06-20 18:12:00 +0200

    test: buscar por tag

--- src/test/gifs.test.ts
+++ src/test/gifs.test.ts
@@ -1,65 +1,77 @@
-import { createApp } from "../app"
-import { createMemoryDB } from "../services/dbService"
-import request from "supertest"
-import { Express, response } from "express"
-import { Gif } from "../interfaces/Gif"
+import { createApp } from "../app";
+import { createMemoryDB } from "../services/dbService";
+import request from "supertest";
+import { Express, response } from "express";
+import { Gif } from "../interfaces/Gif";
 
 describe("Gifs endpoint", function () {
-  let app: Express
+  let app: Express;
 
   beforeEach(() => {
-    const db = createMemoryDB("../fixtures/db.json")
-    app = createApp(db)
-  })
+    const db = createMemoryDB("../fixtures/db.json");
+    app = createApp(db);
+  });
 
-  it("exists", async () => await request(app).get("/api/gifs").expect(200))
+  it("exists", async () => await request(app).get("/api/gifs").expect(200));
 
   it("return json", async () =>
     await request(app)
       .get("/api/gifs")
       .expect(200)
-      .expect("Content-Type", /application\/json/))
+      .expect("Content-Type", /application\/json/));
 
   it("response is array", async () => {
-    const response = await request(app).get("/api/gifs").expect(200)
-    expect(response.body).toBeInstanceOf(Array)
-  })
+    const response = await request(app).get("/api/gifs").expect(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
 
   it("response returns 50 gifs", async () => {
-    const response = await request(app).get("/api/gifs").expect(200)
-    expect(response.body).toHaveLength(50)
-  })
+    const response = await request(app).get("/api/gifs").expect(200);
+    expect(response.body).toHaveLength(50);
+  });
 
   it("endpoint 'gifs/search/' exists", async () => {
     const response = await request(app)
       .get("/api/gifs/search?searchedText=a")
-      .expect(200)
-  })
+      .expect(200);
+  });
 
   it("can search and have results limited to 50", async () => {
     const response = await request(app)
       .get("/api/gifs/search?searchedText=a")
-      .expect(200)
-    expect(response.body.length).toBeLessThanOrEqual(50)
-  })
+      .expect(200);
+    expect(response.body.length).toBeLessThanOrEqual(50);
+  });
 
   it("can search and results contains the searched text (cat)", async () => {
     const response = await request(app)
       .get("/api/gifs/search?searchedText=cat")
-      .expect(200)
-    expect(response.body.length).toBeGreaterThanOrEqual(1)
+      .expect(200);
+    expect(response.body.length).toBeGreaterThanOrEqual(1);
     response.body.forEach((gif: Gif) => {
-      expect(gif.title.toLocaleLowerCase()).toContain("cat")
-    })
-  })
+      expect(gif.title.toLocaleLowerCase()).toContain("cat");
+    });
+  });
 
   it("can search and results contains the searched text (dog)", async () => {
     const response = await request(app)
       .get("/api/gifs/search?searchedText=dog")
-      .expect(200)
-    expect(response.body.length).toBeGreaterThanOrEqual(1)
+      .expect(200);
+    expect(response.body.length).toBeGreaterThanOrEqual(1);
     response.body.forEach((gif: Gif) => {
-      expect(gif.title.toLocaleLowerCase()).toContain("dog")
-    })
-  })
-})
+      expect(gif.title.toLocaleLowerCase()).toContain("dog");
+    });
+  });
+
+  it("can search and results contains the searched tag #funny", async () => {
+    const response = await request(app)
+      .get("/api/gifs/search?searchedText=#funny")
+      .expect(200);
+    expect(response.body.length).toBeGreaterThanOrEqual(1);
+    response.body.forEach((gif: Gif) => {
+      expect(gif.tags.map((tag) => tag.toLocaleLowerCase())).toContain(
+        "#funny"
+      );
+    });
+  });
+});
