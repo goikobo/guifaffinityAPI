1f859a25395875ba4e03287b33172ef225e3057a
javiducun <javiducun@gmail.com>
2023-07-26 18:30:36 +0200

    refactor add describe to test

--- src/test/gifs.test.ts
+++ src/test/gifs.test.ts
@@ -12,98 +12,107 @@ describe("Gifs endpoint", function () {
     app = createApp(db)
   })
 
-  it("exists /api/gifs", async () =>
-    await request(app).get("/api/gifs").expect(200))
+  describe("/api/gifs", () => {
+    it("exists", async () => await request(app).get("/api/gifs").expect(200))
 
-  it("return json", async () =>
-    await request(app)
-      .get("/api/gifs")
-      .expect(200)
-      .expect("Content-Type", /application\/json/))
+    it("return json", async () =>
+      await request(app)
+        .get("/api/gifs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/))
 
-  it("response is array", async () => {
-    const response = await request(app).get("/api/gifs").expect(200)
-    expect(response.body).toBeInstanceOf(Array)
-  })
+    it("response is array", async () => {
+      const response = await request(app).get("/api/gifs").expect(200)
+      expect(response.body).toBeInstanceOf(Array)
+    })
 
-  it("response returns 50 gifs", async () => {
-    const response = await request(app).get("/api/gifs").expect(200)
-    expect(response.body).toHaveLength(50)
-  })
-
-  it("endpoint 'gifs/search/' exists", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=a")
-      .expect(200)
-  })
-
-  it("can search and have results limited to 50", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=a")
-      .expect(200)
-    expect(response.body.length).toBeLessThanOrEqual(50)
-  })
-
-  it("can search and results contains the searched text (cat)", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=cat")
-      .expect(200)
-    expect(response.body.length).toBeGreaterThanOrEqual(1)
-    response.body.forEach((gif: Gif) => {
-      expect(gif.title.toLocaleLowerCase()).toContain("cat")
+    it("response returns 50 gifs", async () => {
+      const response = await request(app).get("/api/gifs").expect(200)
+      expect(response.body).toHaveLength(50)
     })
   })
 
-  it("can search and results contains the searched text (dog)", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=dog")
-      .expect(200)
-    expect(response.body.length).toBeGreaterThanOrEqual(1)
-    response.body.forEach((gif: Gif) => {
-      expect(gif.title.toLocaleLowerCase()).toContain("dog")
+  describe("gifs/search/", () => {
+    it("endpoint exists", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=a")
+        .expect(200)
+    })
+
+    it("can search and have results limited to 50", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=a")
+        .expect(200)
+      expect(response.body.length).toBeLessThanOrEqual(50)
+    })
+
+    it("can search and results contains the searched text (cat)", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=cat")
+        .expect(200)
+      expect(response.body.length).toBeGreaterThanOrEqual(1)
+      response.body.forEach((gif: Gif) => {
+        expect(gif.title.toLocaleLowerCase()).toContain("cat")
+      })
+    })
+
+    it("can search and results contains the searched text (dog)", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=dog")
+        .expect(200)
+      expect(response.body.length).toBeGreaterThanOrEqual(1)
+      response.body.forEach((gif: Gif) => {
+        expect(gif.title.toLocaleLowerCase()).toContain("dog")
+      })
+    })
+
+    it("can search and results return 404 if not contains any result (Supercalifragilisticoexpialidoso)", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=Supercalifragilisticoexpialidoso")
+        .expect(200)
+      expect(response.body).toHaveLength(0)
+    })
+
+    it("can search and results contains the searched tag #funny", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=" + encodeURIComponent("#funny"))
+        .expect(200)
+      expect(response.body.length).toBeGreaterThanOrEqual(1)
+      response.body.forEach((gif: Gif) => {
+        expect(gif.tags.map((tag) => tag.toLocaleLowerCase())).toContain(
+          "#funny"
+        )
+      })
+    })
+
+    it("can search and results contains the searched tag #movie", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=" + encodeURIComponent("#movie"))
+        .expect(200)
+      expect(response.body.length).toBeGreaterThanOrEqual(1)
+      response.body.forEach((gif: Gif) => {
+        expect(gif.tags.map((tag) => tag.toLocaleLowerCase())).toContain(
+          "#movie"
+        )
+      })
+    })
+
+    it("can search and results contains the searched tag #goiko", async () => {
+      const response = await request(app)
+        .get("/api/gifs/search?searchedText=" + encodeURIComponent("#goiko"))
+        .expect(200)
+      expect(response.body).toHaveLength(0)
     })
   })
 
-  it("can search and results return 404 if not contains any result (Supercalifragilisticoexpialidoso)", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=Supercalifragilisticoexpialidoso")
-      .expect(200)
-    expect(response.body).toHaveLength(0)
+  describe("/api/gifs/getById", () => {
+    it("exists /api/gifs/getById", async () =>
+      await request(app).get("/api/gifs/getById").expect(200))
+
+    it("return json on /api/gifs/getById", async () =>
+      await request(app)
+        .get("/api/gifs/getById")
+        .expect(200)
+        .expect("Content-Type", /application\/json/))
   })
-
-  it("can search and results contains the searched tag #funny", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=" + encodeURIComponent("#funny"))
-      .expect(200)
-    expect(response.body.length).toBeGreaterThanOrEqual(1)
-    response.body.forEach((gif: Gif) => {
-      expect(gif.tags.map((tag) => tag.toLocaleLowerCase())).toContain("#funny")
-    })
-  })
-
-  it("can search and results contains the searched tag #movie", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=" + encodeURIComponent("#movie"))
-      .expect(200)
-    expect(response.body.length).toBeGreaterThanOrEqual(1)
-    response.body.forEach((gif: Gif) => {
-      expect(gif.tags.map((tag) => tag.toLocaleLowerCase())).toContain("#movie")
-    })
-  })
-
-  it("can search and results contains the searched tag #goiko", async () => {
-    const response = await request(app)
-      .get("/api/gifs/search?searchedText=" + encodeURIComponent("#goiko"))
-      .expect(200)
-    expect(response.body).toHaveLength(0)
-  })
-
-  it("exists /api/gifs/getById", async () =>
-    await request(app).get("/api/gifs/getById").expect(200))
-
-  it("return json on /api/gifs/getById", async () =>
-    await request(app)
-      .get("/api/gifs/getById")
-      .expect(200)
-      .expect("Content-Type", /application\/json/))
 })
